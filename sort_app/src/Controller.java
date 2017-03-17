4f2915949d645c05b19ffb8a4aaa7b728e2287f5
samikroon <samikroon@gmail.com>
2017-03-17 16:21:08 +0100

    Make sort algorithm compatible with controller

--- sort_app/src/Controller.java
+++ sort_app/src/Controller.java
@@ -1,8 +1,69 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ResourceBundle;
 
 /**
  * Created by samikroon on 3/15/17.
  */
-public class Controller {
+public class Controller implements Initializable{
+
+    @FXML
+    private Canvas canvas;
+    public GraphicsContext g;
+    private Model model;
+
+    private static int[] xValues = {0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300,
+                                    320, 340, 360, 380, 400};
+
+
+
+    private int widthObjects = 18;
+
+
+
+    //fillRect(double x, double y, double w, double h
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        g = canvas.getGraphicsContext2D();
+
+        this.model = new Model();
+
+
+        g.setFill(Color.LIGHTBLUE);
+        g.fillRect(0, 0, 400, 400);
+        g.save();
+        g.setFill(Color.DARKGREY);
+        g.fillRect(0, 380, 18, 20);
+        g.fillRect(20, 360, 18, 40);
+        g.fillRect(40, 340, 18, 60);
+        g.fillRect(60, 320, 18, 80);
+        g.fillRect(80, 300, 18, 100);
+        g.fillRect(100, 280, 18, 120);
+
+
+    }
+
+    @FXML
+    public void reset () {
+        g.fillRect(120, 260, 18, 140);
+    }
+
+    @FXML
+    public void step() {
+        model.bubbleStep();
+        System.out.println("\n");
+    }
+
+
+
 
 }
--- sort_app/src/Model.java
+++ sort_app/src/Model.java
@@ -4,40 +4,51 @@ import java.util.Random;
  * Created by leonv on 15-3-2017.
  */
 public class Model {
-    private int[] list = new int[10];
-    public  boolean notSorted = true;
+    private int[] sortableObjects = {20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300,
+            320, 340, 360, 380, 400};
 
-    public Model(int[] list) {
-        this.list = list;
-        int i;
-        Random num = new Random();
-
-        for(i=0; i<10; i++){
-            list[i] = num.nextInt(100);
-        }
+    public Model() {
+        shuffle(sortableObjects);
     }
 
     public boolean bubbleStep(){
         int i;
-        for(i=0; i<this.list.length; i++){
-            if(this.list[i] > this.list[i+1]){
-                int x = this.list[i];
-                this.list[i] = this.list[i+1];
-                this.list[i+1] = x;
-                System.out.println("Swap!\n");
-                this.print();
+        for(i=0; i<this.sortableObjects.length - 1; i++){
+            if(this.sortableObjects[i] > this.sortableObjects[i+1]){
+                int x = this.sortableObjects[i];
+                this.sortableObjects[i] = this.sortableObjects[i+1];
+                this.sortableObjects[i+1] = x;
+                print();
                 return true;
             }
-            System.out.println("No Swap!\n");
         }
-        this.print();
-        System.out.println("Sorted :)\n");
+        print();
         return false;
     }
 
     public void print() {
-        for(int i=0; i<this.list.length; i++){
-            System.out.println(this.list[i]);
+        for(int i=0; i<this.sortableObjects.length; i++){
+            System.out.println(this.sortableObjects[i]);
         }
     }
+
+
+
+    public int[] getSortableObjects() {
+        return sortableObjects;
+    }
+
+    //Fisher-Yates shuffle; source: https://www.dotnetperls.com/shuffle-java
+    static void shuffle(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < array.length; i++) {
+            // Get a random index of the array past i.
+            int random = i + (int) (Math.random() * (n - i));
+            // Swap the random element with the present element.
+            int randomElement = array[random];
+            array[random] = array[i];
+            array[i] = randomElement;
+        }
+    }
+
 }
