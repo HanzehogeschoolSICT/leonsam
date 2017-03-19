9555c406acfb3c4acc2f4635c61883d9aa869ced
samikroon <samikroon@gmail.com>
2017-03-19 22:02:56 +0100

    get interval option working and add choicebox for multiple sort algortihms

--- sort_app/src/Controller.java
+++ sort_app/src/Controller.java
@@ -2,26 +2,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-
 import javax.swing.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.text.ParseException;
 import java.util.ResourceBundle;
 
 /**
  * Created by samikroon on 3/15/17.
  */
-public class Controller implements Initializable{
+public class Controller extends Thread implements Initializable {
 
-    @FXML
-    private Canvas canvas;
+    @FXML private Canvas canvas;
+    @FXML private ChoiceBox<String> choicebox;
+    @FXML private TextField sizeInterval;
     public GraphicsContext g;
     private Model model;
+    private long interval;
+    private Thread thread;
+    public static volatile boolean killFlag = false;
 
-    
+
 
 
     //fillRect(double x, double y, double w, double h
@@ -29,14 +32,10 @@ public class Controller implements Initializable{
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         g = canvas.getGraphicsContext2D();
-
         this.model = new Model();
-
-
         repaint();
-
-
     }
+
 
     public void repaint() {
         int[] data = model.getSortableObjects();
@@ -58,12 +57,88 @@ public class Controller implements Initializable{
 
     @FXML
     public void step() {
-        boolean notDone;
-        notDone = model.bubbleStep();
-        if (notDone == false) {
-            JOptionPane.showMessageDialog(null, "Shit is sorted", "Done", JOptionPane.INFORMATION_MESSAGE);
+
+        switch (choicebox.getValue()) {
+
+            case "bubblesort":
+                boolean notDone;
+                notDone = model.bubbleStep();
+                if (notDone == false) {
+                    JOptionPane.showMessageDialog(null, "Shit is sorted", "Done", JOptionPane.INFORMATION_MESSAGE);
+                }
+                repaint();
+                break;
+
+            case "insertionsort":
+                break;
+
+            case "quicksort":
+                break;
+
         }
-        repaint();
+
+    }
+
+    @FXML
+    public void startWithInterval() {
+        this.interval = 100;
+        String tf = sizeInterval.getText();
+        if (!tf.equals("")) {
+            try {
+                this.interval = Long.parseLong(tf);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "You did not fill in a number dumbass", "Done", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+        }
+
+
+        thread = new Thread() {
+          public void run() {
+              boolean notDone = true;
+
+              while (notDone) {
+
+                  switch (choicebox.getValue()) {
+
+                      case "bubblesort":
+                          notDone = model.bubbleStep();
+                          break;
+
+                      case "insertionsort":
+                          break;
+
+                      case "quicksort":
+                          break;
+
+                  }
+
+
+                  repaint();
+                  try {
+                      sleep(getInterval());
+                  } catch (InterruptedException e) {
+                      return;
+                  }
+              }
+
+              JOptionPane.showMessageDialog(null, "Shit is sorted", "Done", JOptionPane.INFORMATION_MESSAGE);
+
+          }
+        };
+
+        thread.start();
+
+
+
+    }
+
+    public void stopIntervalThread() {
+        thread.interrupt();
+    }
+
+    public long getInterval() {
+        return interval;
     }
 
 }
