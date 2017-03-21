56f6bfe0a22f4b96753572313b78916d4573d378
samikroon <samikroon@gmail.com>
2017-03-21 12:24:12 +0100

    add wordlist

--- boggle/src/Controller.java
+++ boggle/src/Controller.java
@@ -1,2 +1,29 @@
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML private Canvas canvas;
+    @FXML private ListView<String> listView;
+    public GraphicsContext g;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        g = canvas.getGraphicsContext2D();
+        repaint();
+    }
+
+    public void repaint() {
+        g.setFill(Color.LIGHTBLUE);
+        g.fillRect(0, 0, 500, 500);
+
+    }
+
 }
