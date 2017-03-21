b69456cce7a5f54ec4645f4305b5ae996f1d62c7
samikroon <samikroon@gmail.com>
2017-03-21 11:16:02 +0100

    add boggle module to repository

--- sort_app/src/Main.java
+++ sort_app/src/Main.java
@@ -21,11 +21,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        //int[] myList = new int[10];
-        //Model x = new Model(myList);
         launch(args);
-        //while(x.notSorted){
-        //    x.bubbleStep();
-        //}
+
     }
 }
