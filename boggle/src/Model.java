c2638d107d7879459a93531be9537b83d12ff8e3
Leon <leonvisscher@home.nl>
2017-03-22 11:00:20 +0100

    Begin Model class

---
+++ boggle/src/Model.java
@@ -0,0 +1,21 @@
+import sun.reflect.generics.tree.Tree;
+
+import java.io.FileReader;
+import java.util.TreeSet;
+
+/**
+ * Created by leonv on 22-3-2017.
+ */
+public class Model {
+private TreeSet<String> treeSet = new TreeSet();
+
+    public Model(){
+
+    }
+
+    public void dictonaryBuilder(){
+        try{
+            FileReader fileReader = new FileReader();
+        }
+    }
+}
